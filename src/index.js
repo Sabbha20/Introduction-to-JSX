f8231ea1d4a30a82567a8c13f9169a94a85009f5
Sabbha Mondal <62327447+Sabbha20@users.noreply.github.com>
2024-08-02 13:23:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 
-ReactDom.render(
-  <h1>Hello, this is Sabbha</h1>,
+const luckyNumber = 6;
+const fname = "Sabbha";
+const lname = "Mondal";
+
+ReactDOM.render(
+  <>
+    <h1>Hello, this is Sabbha</h1>
+    <h4>Full Name: {`${fname} ${lname}`}</h4>
+    <h4>
+      Full Name: {fname} {lname}
+    </h4>
+    <h4>Full Name: {fname + " " + lname}</h4>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+    <ol>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ol>
+    <p>My lucky number: {luckyNumber}</p>
+    <p>My random number: {Math.floor(Math.random() * 10)}</p>
+    <p>
+      &copy; {fname} {lname}, {new Date().getFullYear()}
+    </p>
+  </>,
   document.getElementById("root")
 );
