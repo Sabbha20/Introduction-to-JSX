0e694e2ca050920ecf93116d9d0da3c1248f53ac
Sabbha20 <sabbha.power1@gmail.com>
2024-08-03 23:02:56 +0530

    Local repo updation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const luckyNumber = 6;
+const fname = "Sabbha";
+const lname = "Mondal";
+const randomImg = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      Hello, this is Sabbha
+    </h1>
+    <h4>Full Name: {`${fname} ${lname}`}</h4>
+    <h4>
+      Full Name: {fname} {lname}
+    </h4>
+    <h4>Full Name: {fname + " " + lname}</h4>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+    <ol>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ol>
+    <p>My lucky number: {luckyNumber}</p>
+    <p>My random number: {Math.floor(Math.random() * 10)}</p>
+    <hr />
+    <div>
+      <img
+        className="food-img"
+        src="https://www.dosafactorymenu.com/blog-admin/images/indian-food-items-apt-for-everyone041641.jpeg"
+        alt="sabji-chapati"
+      />
+      <img
+        className="food-img"
+        src="https://images.news18.com/ibnlive/uploads/2021/01/1610716314_untitled-design-2021-01-15t184025.049.jpg"
+        alt="pizza"
+      />
+      <img
+        className="food-img"
+        src="https://images.herzindagi.info/image/2021/Sep/chaii-samosa.jpg"
+        alt="samosa-chai"
+      />
+      <img className="food-img" src={randomImg} alt="random-img" />
+    </div>
+    <hr />
+    <p>
+      &copy; {fname} {lname}, {new Date().getFullYear()}
+    </p>
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
